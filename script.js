bd11916c3b6f0340ca20951cbf625e816ab4b911
def_fun <131314688+insane-infants@users.noreply.github.com>
2025-01-12 12:49:23 +0500

    basemap changed

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 var map = L.map('map').setView([0, 0], 1);
 
-var Stadia_StamenTonerBackground = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_background/{z}/{x}/{y}{r}.{ext}', {
-  minZoom: 1,
-  maxZoom: 20,
-  attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  ext: 'png'
+var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 });
-Stadia_StamenTonerBackground.addTo(map);
+Esri_WorldImagery.addTo(map);
+
 var cN = []
 for (const feature of countries.features) {
   cN.push(feature.properties.ADMIN);
